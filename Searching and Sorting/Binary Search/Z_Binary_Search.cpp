9719523b8d719a3cd65cbcd9bac56f47693b8419
Mystic <187439614+StriverV@users.noreply.github.com>
2025-04-27 22:07:34 +0600

    Update Z_Binary_Search.cpp

--- Searching and Sorting/Binary Search/Z_Binary_Search.cpp
+++ Searching and Sorting/Binary Search/Z_Binary_Search.cpp
@@ -1,28 +1,42 @@
-class Solution {
-    public:
-        int search(vector<int>& nums, int target) {
-         
-            int n = nums.size();
-            int lp = 0, rp = n-1;
-            bool flag = 0;
-            int idx = -1;
-            while( lp <= rp)          // O(logn)
-            {
-                int mid = (lp + rp)/2;
-    
-                if(nums[mid] == target  )  
-                {
-                    flag = 1;
-                    idx = mid;
-                    break;
-                }   
-    
-                else if(target  < nums[mid])   rp = mid - 1;
-    
-                else   lp = mid + 1;
-            }
-    
-            return idx;
-        }
+#include<bits/stdc++.h>
+#define Naba ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll long long 
+#define nl '\n'
+using namespace std;
 
-    };
+int main()
+{
+    Naba;
+    int n;
+    cin >> n;
+    vector<int>v(n);
+
+    for(int i = 0; i  < n; i++)
+        cin >> v[i];
+            
+    int target;
+    cin >> target;
+    
+    int lp = 0, rp = n-1;
+    bool flag = 0;
+    int idx = -1;
+    while( lp <= rp)          // O(logn)
+    {
+        int mid = (lp + rp)/2;
+    
+        if(v[mid] == target  )  
+        {
+            flag = 1;
+            idx = mid;
+            break;
+        }   
+    
+        else if(target  < v[mid])   rp = mid - 1;
+    
+        else   lp = mid + 1;
+    }
+
+    flag? cout << "Found" << nl: cout << "Not Found" << nl;
+
+    return 0;
+}
