028edec08e6cb7bb0c140e95e71db1822cf708fa
Mystic <187439614+StriverV@users.noreply.github.com>
2025-05-02 22:57:57 +0600

    Add files via upload

---
+++ STL/Vector/Vector.cpp
@@ -0,0 +1,59 @@
+
+
+// ******** vector ********
+//int n;  cin >> n;
+//vector<int> v(n);
+//for (int i = 0;i < n; i++) 
+//    cin >> v[i];
+
+
+// int n;  cin >> n;
+// vector<int> v;
+// for (int i = 0;i < n;i++)
+// {
+//     int x;  cin >> x;
+//     v.push_back(x);
+// }
+
+
+// for (auto i : v) 
+// {
+//     cout << i << " ";
+// }
+// cout << nl;
+
+
+// vis.assign(10005, 0);
+// int n; 
+// cin >> n;
+// vector<int> vis;
+// vis.assign(n, 0);
+// vis.assign(n, -1);
+
+// for (int i = 0;i < n;i++)
+//    cout << vis[i] << " ";
+// cout << nl;
+
+// v.resize(5);
+// for (int i = 0;i < v.size();i++)
+//    cout << v[i] << " ";
+// cout << nl;
+
+// reverse(v.begin(), v.end());
+// sort(v.begin(), v.end());
+
+// auto mn = min_element(v.begin(), v.end());
+// auto mx = max_element(v.begin(), v.end());
+// cout << *mn << '\n';
+// int maxElementPosition = mx - v.begin();
+
+// auto lastElement = --v.end();
+// cout << *lastElement << nl;
+// v.clear();
+// cout << v.empty() << nl;
+// cout << v.size() << nl;
+// v.pop_back();
+// cout << v.front() <<nl;
+// cout << v.back() <<nl;
+
+
