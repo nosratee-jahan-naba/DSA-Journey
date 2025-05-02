570efb3c5cd11a125d453444b25569f79934c180
Mystic <187439614+StriverV@users.noreply.github.com>
2025-05-03 00:45:48 +0600

    Create set.cpp

---
+++ STL/set.cpp
@@ -0,0 +1,63 @@
+
+#include<bits/stdc++.h>
+#define Naba ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define nl '\n'
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+using namespace std;
+
+int main()
+{
+    Naba;
+  
+    // set keeps all elements sorted automatically
+    // Duplicates are ignored
+    
+    // int n;
+    // cin >> n;
+    // set<int> st;
+    // for (int i = 1;i <= n;i++) 
+    // {
+    //     int x;
+    //     cin >> x;
+    //     st.insert(x);
+    // }
+     
+
+    // Indexing is not allowed directly so,
+    // auto it = s.begin();
+    // it++;
+    // it++;
+    // it++;
+    // it--;
+    // cout << *it << '\n';
+    
+    // for (auto i : st) 
+    // {
+    //     cout << i << " ";
+    // }
+    // cout << nl;
+    
+    // auto it = st.find(25);
+    // it != st.end()  ?  yes : no;
+    // st.count(4) ? yes : no;
+    // s.erase(6);
+    
+    
+    int x; cin >> x;
+    //------ upper_bound(x): Finds the x ,else first element > x.
+
+    // auto it = st.lower_bound(x)
+    // it == st.end() ? cout << *it << nl :cout << *it << nl;
+
+    //----- upper_bound(x): Finds the first element > x.
+
+    // auto it = st.upper_bound(x);
+    // it == st.end() ? cout << *it << nl :cout << *it << nl;
+    
+
+    //    return 0;
+    // }
+
+    return 0;
+}
