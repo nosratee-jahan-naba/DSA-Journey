7285e297a29b497577ceb6ad9825a25def954417
BeMe <187439614+soloflare1@users.noreply.github.com>
2025-10-08 18:41:20 +0600

    Update vector.cpp

--- STL/vector.cpp
+++ STL/vector.cpp
@@ -1,4 +1,13 @@
 #include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t; cin >> t;
+    while(t--){
+        vector<int>
+    }
+    return 0;
+}
+#include<bits/stdc++.h>
 #define Naba ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
 #define nl endl
 #define ll long long
